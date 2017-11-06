0bd1ecd1203f11e36b8a5e4222ea995e6e007b9c
lkfo415579 <lkfo415579@hotmail.com>
2017-11-06 17:03:09 +0800

    ss

--- update.sh
+++
@@ -1,5 +0,0 @@
-mv bin/rd_util/ bin/jieba/ bin/langdetect/ ./
-rm -rf bin/*
-mv AA/server_batch/* bin/
-mv jieba/ langdetect/ rd_util/ bin/
-rm -r AA/
--- util.sh
+++
@@ -1,8 +0,0 @@
-cat valid.log | grep cross | sort -g -k8,8 -t ' ' | cut -f 4 -d ' ' | head -n 4 | xargs -I {} echo model_revo_s2s.iter{}.npz
-scp  revo@10.0.100.182:/home/revo/workshop/com_best_deep/tester
-
-#fast align
-perl prepare-fast-align.perl ~/workshop/competition/world/train.clean.en ~/workshop/competition/world/train.clean.zh > train.en-zh
-./fast_align -i train.en-zh -d -o -v > forward.align
-./fast_align -i train.en-zh -d -o -v -r > reverse.align
-./old_atools -i forward.align -j reverse.align -c grow-diag-final-and > grow-diag-final-and
